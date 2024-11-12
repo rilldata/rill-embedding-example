6efa5c0cb9b0fc9711ae83937b0f511753da41be
royendo <67675319+royendo@users.noreply.github.com>
2024-11-12 09:01:25 -0700

    remove token.(again)

--- pages/api/iframe-attributes.js
+++ pages/api/iframe-attributes.js
@@ -1,6 +1,5 @@
 // Get the secret Rill service token from an environment variable.
-//const rillServiceToken = process.env.RILL_SERVICE_TOKEN;
-const rillServiceToken = 'rill_svc_7RbpHxRrEWX57L7kE2rFCG3YYElfcUM39rKJbf2S4UBKs0QsOF9yoD'
+const rillServiceToken = process.env.RILL_SERVICE_TOKEN;
 
 // Configure the dashboard to request an iframe URL for.
 // Note that the organization must be the same as the one the service token is associated with.
