c985ef252bca7ca8ebba66b6864e474fce7ba08c
royendo <67675319+royendo@users.noreply.github.com>
2025-04-09 10:19:43 -0600

    keep forgetting

--- src/app/api/get-iframe/route.tsx
+++ src/app/api/get-iframe/route.tsx
@@ -3,8 +3,7 @@
 export async function POST(req: Request) {
     const { org, project, body } = await req.json();
 
-    // const rillServiceToken = process.env.RILL_SERVICE_TOKEN!;
-    const rillServiceToken = 'rill_svc_8UoW8cLAgHyqSJKCV9eNCejETlGhrZYuAGxFhnlKvC8zyyEadeE5RI'
+    const rillServiceToken = process.env.RILL_SERVICE_TOKEN!;
     const url = `https://admin.rilldata.com/v1/organizations/${org}/projects/${project}/iframe`;
 
     const response = await fetch(url, {
